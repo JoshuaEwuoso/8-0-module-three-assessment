6c71557fc0d5a51e46edef0fa09712fd4bb6af8b
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-16 13:53:31 -0500

    changed logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,7 +9,7 @@ class NavBar extends React.Component {
                 <Link to="/">
                     <img
                         className="logo"
-                        src="https://cdn.onebauer.media/one/media/5fac/0224/ceb4/e33d/d05b/2527/howls-moving-castle.jpg?format=jpg&quality=80&width=960&height=540&ratio=16-9&resize=aspectfill"
+                        src="https://thedailyaztec.com/wp-content/uploads/2020/01/SG-1-e1580191326367.jpg"
                         alt="logo"
                     />
                 </Link>
