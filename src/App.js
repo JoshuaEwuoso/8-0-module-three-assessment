b29cbb3bebfa96ae915dd5d7e379848cb8da7276
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-12-16 11:01:38 -0500

    adds styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Locations from "./Components/Locations";
+import Movies from "./Components/Movies";
+import NavBar from "./Components/NavBar";
+import People from "./Components/People";
 
-function App() {
-  return (
-    <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/people" component={People} />
+          <Route path="/locations" component={Locations} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
